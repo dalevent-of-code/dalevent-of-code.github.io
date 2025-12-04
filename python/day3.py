335d296177852d7b0e3d3d83cbc1a68178bf6ba7
hotcoldvote <hotcoldvote@gmail.com>
2025-12-04 06:07:55 +0000

    fein4

--- python/day3.py
+++ python/day3.py
@@ -6,37 +6,23 @@ def run(event):
     lines = raw_input.splitlines()
     lines = [line.strip() for line in lines]
 
-    count = 0
+    def day3(part):
+        count = 0
+        for _, line in (enumerate(lines)):
+            bank = list(line)
+            bank = [int(cell) for cell in bank]
+            maxes = []
+            marker = 0
+            for i in range(-(part+1), 0):
+                maxi = max(bank[marker:i])
+                maxes.append(maxi)
+                marker += bank[marker:i].index(maxi) + 1    
+            maxes.append(max(bank[marker:])) 
+            count += (int("".join([str(cell) for cell in maxes])))
+        return count
+    document.getElementById("day3-out-1").textContent = str(day3(2))
 
-    for _, line in (enumerate(lines)):
-        bank = list(line)
-        bank = [int(cell) for cell in bank]
-        maxes = []
-        marker = 0
-        for i in range(-1, 0):
-            maxi = max(bank[marker:i])
-            maxes.append(maxi)
-            marker += bank[marker:i].index(maxi) + 1    
-        maxes.append(max(bank[marker:])) 
-        count += (int("".join([str(cell) for cell in maxes])))
-
-    document.getElementById("day3-out-1").textContent = str(count)
-    
-    count = 0
-    
-    for _, line in (enumerate(lines)):
-        bank = list(line)
-        bank = [int(cell) for cell in bank]
-        maxes = []
-        marker = 0
-        for i in range(-11, 0):
-            maxi = max(bank[marker:i])
-            maxes.append(maxi)
-            marker += bank[marker:i].index(maxi) + 1    
-        maxes.append(max(bank[marker:])) 
-        count += (int("".join([str(cell) for cell in maxes])))
-
-    document.getElementById("day3-out-2").textContent = str(count)
+    document.getElementById("day3-out-2").textContent = str(day3(12))
     
 run_proxy = create_proxy(run)
 document.getElementById("day3-btn").addEventListener("click", run_proxy)
